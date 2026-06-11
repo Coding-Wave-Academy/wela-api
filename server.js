0fdf11c934b5228049a2fd8aa6425831d22e949a
Ribert Kandi junior <90086835+Coding-Wave-Academy@users.noreply.github.com>
2026-06-11 03:58:48 +0100

    add

--- server.js
+++ server.js
@@ -17,3 +17,60 @@ app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
+
+app.post("/create-case", async (req, res) => {
+
+  try {
+
+    const args =
+      req.body.message.toolCalls[0]
+      .function.arguments;
+
+    const emergencyCaseId =
+      "WELA-" + Date.now();
+
+    const document =
+      await databases.createDocument(
+        process.env.DATABASE_ID,
+        process.env.COLLECTION_ID,
+        ID.unique(),
+        {
+          childName: args.childName,
+          age: args.age || "",
+          description: args.description || "",
+          lastSeenLocation:
+            args.lastSeenLocation || "",
+          lastSeenTime:
+            args.lastSeenTime || "",
+          callerNumber:
+            args.callerNumber || "",
+          language:
+            args.language || "english",
+
+          caseStatus: "active",
+          source: "voice_call",
+
+          emergencyCaseId,
+
+          createdAt:
+            new Date().toISOString()
+        }
+      );
+
+    return res.json({
+      success: true,
+      caseId: emergencyCaseId,
+      documentId: document.$id
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+
+});
